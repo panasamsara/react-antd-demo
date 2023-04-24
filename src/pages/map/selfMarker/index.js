8c8c595d2ca72c4165fccb90162153c855c7c9ea
joshua <panasamsara@126.com>
2023-04-24 10:22:06 +0800

    feat： 多个自定义marker

--- src/pages/map/selfMarker/index.js
+++ src/pages/map/selfMarker/index.js
@@ -6,6 +6,7 @@ import MARKER_SVG from "./marker.svg";
 export default function App() {
   const [center] = useState([116.473179, 39.993169]);
   const [position] = useState([116.473179, 39.993169]);
+  const [position2] = useState([116.463270, 39.983260]);
 
   return (
     <div className="App">
@@ -14,6 +15,11 @@ export default function App() {
           <Marker position={position} offset={[0, -40]} anchor="top-center">
             <img src={MARKER_SVG} alt="marker" />
             <div className="custom-marker">我的坐标是{position.join(", ")}</div>
+          </Marker>
+
+          <Marker position={position2} offset={[0, -40]} anchor="top-center">
+            <img src={MARKER_SVG} alt="marker" />
+            <div className="custom-marker">我的坐标是{position2.join(", ")}</div>
           </Marker>
         </Amap>
       </div>
