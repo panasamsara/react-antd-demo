bb425e9a25b94e8fa768ea906f8aeaba1ecc1c58
joshua <panasamsara@126.com>
2023-04-24 10:18:48 +0800

    feat： 自定义Marker

--- src/Route/Router.js
+++ src/Route/Router.js
@@ -3,10 +3,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TestPage from '../pages/test';
 import HomePage from '../pages/home';
-import MapPage from '../pages/map/mapCountryLayer';
+import MapCountry from '../pages/map/mapCountryLayer';
 import YuanShengGaoDe from '../pages/map/yuanShengGaoDe';
 import MapProvinceLayer from '../pages/map/mapProvinceLayer';
-import MapCarMove from '../pages/map/mapCarMove';
+import MapCarMove from '../pages/map/mapCarMove/mapCarMove';
+import SelfMarker from '../pages/map/selfMarker';
 
 export default function Router() {
   {/* 所有的路由配置均在 BrowserRouter 内部 */}
@@ -16,10 +17,13 @@ export default function Router() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/test' element={<TestPage />} />
-        <Route path='/map' element={<MapPage />} />
-        <Route path='/map2' element={<YuanShengGaoDe />} />
+        <Route path='/mapCountry' element={<MapCountry />} />
+        <Route path='/mapYuanSheng' element={<YuanShengGaoDe />} />
         <Route path='/mapProvince' element={<MapProvinceLayer />} />
+        {/* 轨迹回放 */}
         <Route path='/mapCarMove' element={<MapCarMove />} />
+        {/* 自定义marker */}
+        <Route path='/selfMarker' element={<SelfMarker />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/map/selfMarker/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { Amap, Marker } from "@amap/amap-react";
+import MARKER_SVG from "./marker.svg";
+
+export default function App() {
+  const [center] = useState([116.473179, 39.993169]);
+  const [position] = useState([116.473179, 39.993169]);
+
+  return (
+    <div className="App">
+      <div className="map-container" style={{width: 720, height: 720}}>
+        <Amap zoom={15} center={center}>
+          <Marker position={position} offset={[0, -40]} anchor="top-center">
+            <img src={MARKER_SVG} alt="marker" />
+            <div className="custom-marker">我的坐标是{position.join(", ")}</div>
+          </Marker>
+        </Amap>
+      </div>
+    </div>
+  );
+}
