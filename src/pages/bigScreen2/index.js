9d27205ac7c03cb76227f0825054fc07b9ae33c4
joshua <panasamsara@126.com>
2023-05-06 10:10:15 +0800

    feat: 地图弹层 拖拽 react-draggable

--- src/pages/bigScreen2/index.js
+++ src/pages/bigScreen2/index.js
@@ -153,7 +153,9 @@ export default function App() {
               <ChoseCar />
               <VideoCompo />
           </div>
-          {modalShow ? <DetailModal /> : null}
+          {modalShow 
+          ? <DetailModal /> 
+          : null}
       </div>
     </div>
   );
