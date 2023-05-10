9e3231185a14f77af51e532350cc6ab10e14ccf4
joshua <panasamsara@126.com>
2023-05-10 11:04:41 +0800

    feat: 键盘左右键点击优化

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,23 @@ function App() {
   const navigate = useNavigate();
   // 键盘事件
   const onKeyDown = (e:any) => {
-    switch (window.location.pathname) {
-      case '/bigScreen/page1':
-        navigate('/bigScreen/page2')
-        break;
-      case '/bigScreen/page2':
-        navigate('/bigScreen/page1')
-        break;
-    //   case 37: // 向左事件 - 位置减减
-    //     console.log(222);
-    //     break
-    //   case 39: // 向右事件 - 位置加加
-    //     console.log(333);
-    //     break
+    if(e.keyCode == 37 || e.keyCode == 39){
+      switch (window.location.pathname) {
+        case '/bigScreen/page1':
+          navigate('/bigScreen/page2')
+          break;
+        case '/bigScreen/page2':
+          navigate('/bigScreen/page1')
+          break;
+      //   case 37: // 向左事件 - 位置减减
+      //     console.log(222);
+      //     break
+      //   case 39: // 向右事件 - 位置加加
+      //     console.log(333);
+      //     break
+      }
     }
+    
   }
   useEffect(()=>{
     window.addEventListener('keydown', onKeyDown) // 添加全局事件
