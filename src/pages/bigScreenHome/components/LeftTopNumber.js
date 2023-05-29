cc5b00d3da729d6f97f0997db9d3dc6d3db0deca
joshua <panasamsara@126.com>
2023-05-29 17:07:44 +0800

    feat：组件展示数量修改

--- src/pages/bigScreenHome/components/LeftTopNumber.js
+++ src/pages/bigScreenHome/components/LeftTopNumber.js
@@ -25,9 +25,9 @@ export default function App() {
           background: `url(${getImgUrl('titleBox')}) center no-repeat`, backgroundSize: 'contain',
           color: 'rgb(0,236,252)', textAlign: 'center'
         }}>
-          <div style={{fontSize: 20}}>品牌数量</div>
+          <div style={{fontSize: 20}}>车型数量</div>
           <div style={{display: 'flex', justifyContent: 'center'}}>
-            <div style={{fontSize: 42, color: '#fff'}}>24</div>
+            <div style={{fontSize: 42, color: '#fff'}}>48</div>
             <div style={{position: 'relative', top: 24, marginLeft: 12}}>个</div>
           </div>
         </div>
@@ -36,10 +36,10 @@ export default function App() {
           background: `url(${getImgUrl('titleBox')}) center no-repeat`, backgroundSize: 'contain',
           color: 'rgb(0,236,252)', textAlign: 'center'
         }}>
-          <div style={{fontSize: 20}}>品牌数量</div>
+          <div style={{fontSize: 20}}>行驶总里程</div>
           <div style={{display: 'flex', justifyContent: 'center'}}>
-            <div style={{fontSize: 42, color: '#fff'}}>24</div>
-            <div style={{position: 'relative', top: 24, marginLeft: 12}}>个</div>
+            <div style={{fontSize: 42, color: '#fff'}}>50</div>
+            <div style={{position: 'relative', top: 24, marginLeft: 12}}>亿公里</div>
           </div>
         </div>
 
@@ -47,10 +47,10 @@ export default function App() {
           background: `url(${getImgUrl('titleBox')}) center no-repeat`, backgroundSize: 'contain',
           color: 'rgb(0,236,252)', textAlign: 'center'
         }}>
-          <div style={{fontSize: 20}}>品牌数量</div>
+          <div style={{fontSize: 20}}>行驶总时长</div>
           <div style={{display: 'flex', justifyContent: 'center'}}>
-            <div style={{fontSize: 42, color: '#fff'}}>24</div>
-            <div style={{position: 'relative', top: 24, marginLeft: 12}}>个</div>
+            <div style={{fontSize: 42, color: '#fff'}}>20</div>
+            <div style={{position: 'relative', top: 24, marginLeft: 12}}>亿小时</div>
           </div>
         </div>
         
--- src/pages/bigScreenHome/components/NumberOne.js
+++ src/pages/bigScreenHome/components/NumberOne.js
@@ -13,10 +13,10 @@ export default function App() {
         <div style={{ position: 'relative',width: 300, height: 100,
           color: 'rgb(0,236,252)', textAlign: 'center'
         }}>
-          <div style={{fontSize: 20}}>品牌数量</div>
+          <div style={{fontSize: 20}}>车辆总数</div>
           <div style={{display: 'flex', justifyContent: 'center'}}>
-            <div style={{fontSize: 42, color: '#fff'}}>24</div>
-            <div style={{position: 'relative', top: 24, marginLeft: 12}}>个</div>
+            <div style={{fontSize: 42, color: '#fff'}}>1778</div>
+            <div style={{position: 'relative', top: 24, marginLeft: 12}}>辆</div>
           </div>
         </div>
         
