0c922b5b37bdb7eb949a1e430aeed438f26ff20c
joshua <panasamsara@126.com>
2023-06-01 17:28:00 +0800

    feat: 首屏地图范围

--- src/pages/bigScreenHome/components/mapCountryLayer.js
+++ src/pages/bigScreenHome/components/mapCountryLayer.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
-import { Amap, CountryLayer } from '@amap/amap-react';
+import { Amap, Marker, Polygon, CountryLayer } from '@amap/amap-react';
+import MARKER_SVG from "@/assets/marker.svg";
 
 const COUNTRIES = {
   CHN: {
@@ -24,6 +25,25 @@ export default function RootPage() {
   const [soc, setSoc] = useState('CHN');
   const [opacity, setOpacity] = useState(0.8);
 
+  const path1 = [
+    [126.84, 49.93],
+    [128.09, 49.39],
+    [127.85, 48.87],
+    [126.33, 49.29],
+  ];
+  const path2 = [
+    [121.90, 53.27],
+    [123.11, 53.28],
+    [123.09, 52.68],
+    [122.16, 52.64],
+  ];
+  const path3 = [
+    [124.80, 44.17],
+    [125.83, 43.90],
+    [125.66, 43.45],
+    [124.82, 43.47],
+  ];
+
   return (
     // <div style={{ width: 890, height: 655,}}>
     <div style={{ width: 1920, height: 1010,}}>
@@ -37,6 +57,33 @@ export default function RootPage() {
         showIndoorMap={false} // 不显示室内地图
         mapStyle='amap://styles/f74689d33353c8266c5a7d2f6a98f140'
       >
+        {/* <Polygon
+          path={path1}
+          fillColor= "rgb(202,108,230)"
+        />
+        <Marker position={[128.84, 49.53]} offset={[0, -40]} anchor="top-center">
+            <img src={MARKER_SVG} alt="marker" />
+            <div style={{ width: 120, height: 25, display: 'flex', alignItems: 'center', fontSize: 16, background: '#fff', padding: 10, borderRadius: 4 }}>
+                孙吴，车辆51</div>
+        </Marker>
+        <Polygon
+          path={path2}
+          fillColor= "rgb(6,232,215)"
+        />
+        <Marker position={[123.90, 51.70]} offset={[0, -40]} anchor="top-center">
+            <img src={MARKER_SVG} alt="marker" />
+            <div style={{ width: 120, height: 25, display: 'flex', alignItems: 'center', fontSize: 16, background: '#fff', padding: 10, borderRadius: 4 }}>
+              漠河，车辆63</div>
+        </Marker>
+        <Polygon
+          path={path3}
+          fillColor= "rgb(255,206,55)"
+        />
+        <Marker position={[124.90, 43.57]} offset={[0, -40]} anchor="top-center">
+            <img src={MARKER_SVG} alt="marker" />
+            <div style={{ width: 120, height: 25, display: 'flex', alignItems: 'center', fontSize: 16, background: '#fff', padding: 10, borderRadius: 4 }}>
+              长春，车辆5</div>
+        </Marker> */}
         {/* <CountryLayer
           opacity={opacity}
           depth={2}
